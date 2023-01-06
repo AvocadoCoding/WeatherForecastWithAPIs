08c276a7bd5ecabdebfa1f37d88860fab265cd84
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2023-01-06 23:57:05 +0000

    function added to add buttons to page dynamically from local storage data

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
 let userInput = $("#search-input");
 
+// for loop to create new buttons from local storage on page reload 
+
+
 
 // click functionality added to search button
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     searchInput = $("#search-input").val();
+    // save user entry to local stoarge
     addItem(searchInput);
-
-// save to local stoarge
-// create and append button
-
+    // create and append button
+    appendButton();
 })
 
+// click functionality added to appended buttons
+$(".added-buttons").on("click", function (event) {
+    event.preventDefault();
+    
 
-
+})
 
 
 
@@ -38,14 +44,47 @@ function addItem(add_item) {
        // or tell user it's already there
        console.log(add_item + ' already exists')
     }
-  }
+  };
 
 // Code to prevent a user entering a value with 'enter', button must be clicked
-  $(document).ready(function() {
+$(document).ready(function() {
     $(window).keydown(function(event){
       if(event.keyCode == 13) {
         event.preventDefault();
         return false;
       }
     });
-  });
+});
+
+
+// Function to add buttons to web page  
+function appendButton(){
+
+// Deleting the buttons prior to adding new movies
+  // (this is necessary otherwise there will be repeat buttons)
+  $(".list-group").empty();
+
+  // Take array of locations from local storage and loop through them
+  var locations = JSON.parse(localStorage.getItem("locations"));
+
+  for (var i = 0; i < locations.length; i++) {
+
+    // Then dynamicaly generating buttons for each location in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var newBtn = $("<button>");
+    // Adding a type to the button
+    // newBtn.addType("button");
+    // Adding a class of movie to our button
+    newBtn.addClass("btn btn-secondary btn-block w-100 rounded mt-3 added-buttons");
+    // Adding a data-attribute
+    newBtn.attr("data-name", locations[i]);
+    // Providing the initial button text
+    newBtn.text(locations[i]);
+    // Adding the button to the "list-group" div
+    $(".list-group").append(newBtn);
+  }
+};
+
+
+// Add buttons to page from local storage when page is reloaded
+appendButton();
